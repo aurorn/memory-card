b10d9cd08aaebe8ca80621adf0cdb5035325ffd3
Aurorn <aurornispro@gmail.com>
2025-01-21 18:29:54 -0500

    Changed loading amount

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,3 +1,25 @@
 /* This is where the API call will be made to get the data for the cards */
 
 import Pokedex from 'pokedex-promise-v2'
+
+const P = new Pokedex();
+
+export const fetchPokemonData = async(limit = 20) => {
+    try {
+        const response = await P.getPokemonsList({ limit});
+        const pokemonDetails = await Promise.all(response.results.map(async pokemon => {
+            const details = await P.getPokemonByName(pokemon.name);
+            return {
+                id: details.id,
+                name: details.name,
+                image: details.sprites.front_default
+                // more data if needed
+            };
+    })
+    );
+    return pokemonDetails;
+    } catch (error) {
+        console.error("Error fetching pokemon data", error);
+        return [];
+    }
+};
