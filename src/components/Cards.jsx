bbde74cecd5d5a5579c98b08736fb3a3f6bc80f2
Aurorn <aurornispro@gmail.com>
2025-01-21 18:32:03 -0500

    Imported Card Logic and added in function to create divs and classes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,25 @@
-/* this will be the main component that will render all the cards */
+import { useGameLogic } from './Logic';
+import '../styles/Cards.css';
 
-import { useState } from 'react'
-import '../styles/Cards.css'
+export default function Cards() {
+    const { score, currentCards, handleCardClick } = useGameLogic();
+  
+
+  return (
+    <div className="cards-container">
+      <div className="score">Score: {score}</div>
+      <div className="cards-grid">
+        {currentCards.map((pokemon) => (
+          <div
+            key={pokemon.id}
+            className="card"
+            onClick={() => handleCardClick(pokemon.id)}
+          >
+            <img src={pokemon.image} alt={pokemon.name} />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
