11adac9b5e4075d28d0029ad55a8718e0c0a4783
Aurorn <aurornispro@gmail.com>
2025-01-22 19:12:36 -0500

    Created an intro screen to intitialize the game

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,23 @@
-/* This will be the Intro screen, with 3 differen't difficulty types */
-
-import { useState } from 'react'
+/* This will be the Intro screen, 1 difficulty at the start, easy, medium, hard later*/
 import '../styles/Intro.css'
+import PropTypes from 'prop-types';
+
+
+export default function Intro({ onStart, isGameStarted }) {
+  if (isGameStarted) return null;
+  
+  return (
+    <div className="intro-screen">
+      <h1>Pokemon Memory Game</h1>
+      <p>Don&apos;t click the same pokemon twice!</p>
+      <button className="start-btn" onClick={onStart}>
+        Start Game
+      </button>
+    </div>
+  );
+}
+
+Intro.propTypes = {
+    onStart: PropTypes.func.isRequired,
+    isGameStarted: PropTypes.bool.isRequired
+};
