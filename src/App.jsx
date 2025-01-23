99b39a55e61f659f239732af1b222e783eef665b
Aurorn <aurornispro@gmail.com>
2025-01-22 19:14:56 -0500

    App function intializes the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,45 @@
-import { useState } from 'react'
 
+import { useGameLogic } from './components/Logic';
+import Intro from './components/Intro';
+import Cards from './components/Cards';
+import Alerts from './components/Alerts';
+import Icons from './components/Icons';
 import './App.css'
+import './styles/Alerts.css'
 
+function App() {
+  const { 
+    score, 
+    currentCards, 
+    handleCardClick, 
+    startGame,
+    showAlert,
+    alertMessage,
+    isGameStarted,
+    setShowAlert 
+  } = useGameLogic();
+
+  return (
+    <div className="app">
+
+      <Intro onStart={startGame} isGameStarted={isGameStarted} />
+      <Icons />
+      {isGameStarted && (
+        <>
+          <div className="score">Score: {score}</div>
+          <Cards cards={currentCards} onCardClick={handleCardClick} />
+        </>
+      )}
+      <Alerts 
+        show={showAlert}
+        message={alertMessage}
+        onRestart={() => {
+          setShowAlert(false);
+          startGame();
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
