f3afeec09112c599b598c2bf67c73452521a3940
Aurorn <aurornispro@gmail.com>
2025-01-22 19:11:47 -0500

    Created and Styled a hoverable event to explain the rules of the game

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,3 +2,27 @@
 
 import { useState } from 'react'
 import '../styles/Icons.css'
+
+export default function Icons() {
+  const [showRules, setShowRules] = useState(false);
+
+  return (
+    <div className="icons-container">
+      <div className="icon-wrapper">
+        {showRules && (
+          <div className="rules-tooltip">
+            Click on pokemon cards to score points.
+            Don&apos;t click the same card twice!
+          </div>
+        )}
+        <div 
+          className="help-icon"
+          onMouseEnter={() => setShowRules(true)}
+          onMouseLeave={() => setShowRules(false)}
+        >
+          ?
+        </div>
+      </div>
+    </div>
+  );
+}
