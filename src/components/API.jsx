4ba7ca91c014a7f45c8aad8af2b93330e51b184e
Aurorn <aurornispro@gmail.com>
2025-01-24 18:26:55 -0500

    Added Typing and Colour coordination to the typing

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -4,20 +4,46 @@ import Pokedex from 'pokedex-promise-v2'
 
 const P = new Pokedex();
 
+const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const typeColors = {
+  normal: '#A8A878',
+  fire: '#F08030',
+  water: '#6890F0',
+  electric: '#F8D030',
+  grass: '#78C850',
+  ice: '#98D8D8',
+  fighting: '#C03028',
+  poison: '#A040A0',
+  ground: '#E0C068',
+  flying: '#A890F0',
+  psychic: '#F85888',
+  bug: '#A8B820',
+  rock: '#B8A038',
+  ghost: '#705898',
+  dragon: '#7038F8',
+  dark: '#705848',
+  steel: '#B8B8D0',
+  fairy: '#EE99AC'
+};
+
 export const fetchPokemonData = async(limit = 20) => {
     try {
-        const response = await P.getPokemonsList({ limit});
+        const response = await P.getPokemonsList({ limit });
         const pokemonDetails = await Promise.all(response.results.map(async pokemon => {
             const details = await P.getPokemonByName(pokemon.name);
+            const type = details.types[0].type.name; // Get primary type
             return {
                 id: details.id,
-                name: details.name,
-                image: details.sprites.front_default
-                // more data if needed
+                name: capitalize(details.name),
+                image: details.sprites.front_default,
+                type: type,
+                typeColor: typeColors[type]
             };
-    })
-    );
-    return pokemonDetails;
+        }));
+        return pokemonDetails;
     } catch (error) {
         console.error("Error fetching pokemon data", error);
         return [];
