8d3d7674fe244c842cc9679d284dc86416c26b5d
Aurorn <aurornispro@gmail.com>
2025-01-24 18:27:33 -0500

    Added 3D positioning based on Cursor movements and a Flare effect

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,14 +2,43 @@ import '../styles/Cards.css';
 import PropTypes from 'prop-types';
 
 export default function Cards({ cards, onCardClick }) {
+  const handleMouseMove = (e, cardElement) => {
+    const rect = cardElement.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateY = ((x - centerX) / centerX) * 15;
+    const rotateX = -((y - centerY) / centerY) * 15;
+
+    cardElement.style.transform = 
+      `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+
+    const glare = cardElement.querySelector('.card-glare');
+    if (glare) {
+      const glareX = ((x / rect.width) * 100);
+      const glareY = ((y / rect.height) * 100);
+      glare.style.background = `radial-gradient(circle at ${glareX}% ${glareY}%, rgba(255,255,255,0.3) 0%, rgba(255,255,255,0) 80%)`;
+    }
+  };
+
+  const handleMouseLeave = (cardElement) => {
+    cardElement.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
+  };
+
   return (
     <div className="cards-grid">
       {cards.map((pokemon) => (
         <div
           key={pokemon.id}
           className="card"
+          style={{ background: pokemon.typeColor }}
           onClick={() => onCardClick(pokemon.id)}
+          onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
+          onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
         >
+          <div className="card-glare"></div>
           <img src={pokemon.image} alt={pokemon.name} />
           <p>{pokemon.name}</p>
         </div>
@@ -23,7 +52,9 @@ Cards.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired
+      image: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      typeColor: PropTypes.string.isRequired
     })
   ).isRequired,
   onCardClick: PropTypes.func.isRequired
