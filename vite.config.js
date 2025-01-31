671cf71b9bebdb1e3520cef1b8f2a7c41d4dacca
Aurorn <aurornispro@gmail.com>
2025-01-31 13:45:54 -0500

    added build changes for vite, with a vercel.json to fix prod issues when deploying

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    },
+    sourcemap: true,
+    minify: 'terser'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
